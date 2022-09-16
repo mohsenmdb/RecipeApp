2590c387143b373bcd60d5fd1a1304010a08cda0
Mohse <mohse.mdb@gmail.com>
2022-09-16 14:19:11 +0430

    Asynchronously Load Images with Jetpack Compose

--- app/src/main/java/com/me/recipe/presentation/component/RecipeCard.kt
+++ app/src/main/java/com/me/recipe/presentation/component/RecipeCard.kt
@@ -9,34 +9,40 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.me.recipe.R
 import com.me.recipe.domain.model.Recipe
+import com.me.recipe.util.DEFAULT_RECIPE_IMAGE
+import com.me.recipe.util.LoadPicture
 
 @Composable
 fun RecipeCard(recipe: Recipe, onClick: () -> Unit) {
 
     Card(
         shape = MaterialTheme.shapes.small,
-        elevation = 8.dp,
+        elevation = 4.dp,
         modifier = Modifier
-            .padding(bottom = 6.dp, top = 6.dp)
+            .padding(bottom = 6.dp, top = 6.dp, start = 12.dp, end = 12.dp)
             .fillMaxWidth()
             .clickable(onClick = onClick),
     ) {
         Column(modifier = Modifier.fillMaxWidth()) {
             recipe.featuredImage?.let {
-                Image(
-                    painter = painterResource(id = R.drawable.empty_plate),
-                    contentDescription = "recipe image",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .requiredHeight(225.dp),
-                    contentScale = ContentScale.Crop
-                )
+                val image = LoadPicture(url = it, defaultImage = DEFAULT_RECIPE_IMAGE).value
+                image?.let { img ->
+                    Image(
+                        bitmap = img.asImageBitmap(),
+                        contentDescription = "recipe image",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .requiredHeight(225.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                }
             }
 
             recipe.title?.let { title ->
---
+++ app/src/main/java/com/me/recipe/util/ImageUtils.kt
@@ -0,0 +1,55 @@
+package com.me.recipe.util
+
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.me.recipe.R
+
+const val DEFAULT_RECIPE_IMAGE = R.drawable.empty_plate
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun LoadPicture(
+    url: String,
+    @DrawableRes defaultImage: Int
+): MutableState<Bitmap?> {
+
+    var default : Bitmap? = null
+    val bitmapState = remember { mutableStateOf(default) }
+
+    Glide.with(LocalContext.current)
+        .asBitmap()
+        .load(defaultImage)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                default = resource
+                bitmapState.value = resource
+            }
+            override fun onLoadCleared(placeholder: Drawable?) {
+            }
+
+        })
+
+    Glide.with(LocalContext.current)
+        .asBitmap()
+        .load(url)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                bitmapState.value = resource
+            }
+            override fun onLoadCleared(placeholder: Drawable?) {
+            }
+
+        })
+
+    return bitmapState
+}
