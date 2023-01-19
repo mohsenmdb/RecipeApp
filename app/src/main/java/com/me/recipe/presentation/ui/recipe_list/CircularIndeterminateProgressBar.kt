933cea58d3b6db4c591d8535121fcf0b04dd762c
Mohse <mohse.mdb@gmail.com>
2023-01-19 15:54:14 +0330

    set progressBar in center with ConstraintLayout

--- app/src/main/java/com/me/recipe/presentation/ui/recipe_list/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/me/recipe/presentation/ui/recipe_list/CircularIndeterminateProgressBar.kt
@@ -1,25 +1,39 @@
 package com.me.recipe.presentation.ui.recipe_list
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun CircularIndeterminateProgressBar(isVisible: Boolean) {
     if (isVisible) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            CircularProgressIndicator(color = MaterialTheme.colors.primary)
+        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+            val (progressBar, text) = createRefs()
+            val guideline = createGuidelineFromTop(0.02f)
+            CircularProgressIndicator(
+                modifier = Modifier.constrainAs(progressBar) {
+                    top.linkTo(guideline)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+                color = MaterialTheme.colors.primary
+            )
+
+            Text(
+                text = "Loading...",
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.padding(top = 8.dp).constrainAs(text) {
+                    top.linkTo(progressBar.bottom)
+                    start.linkTo(progressBar.start)
+                    end.linkTo(progressBar.end)
+                }
+            )
         }
     }
 }
