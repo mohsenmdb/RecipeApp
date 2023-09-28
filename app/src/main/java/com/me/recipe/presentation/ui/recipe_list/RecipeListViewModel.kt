acda579623d5dc267d57bba96194df91b49c3947
Mohse <mohse.mdb@gmail.com>
2023-09-28 19:34:53 +0330

    set const vals in companion object

--- app/src/main/java/com/me/recipe/presentation/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/me/recipe/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -24,13 +24,6 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Named
 
-const val PAGE_SIZE = 30
-
-const val STATE_KEY_PAGE = "recipe.state.page.key"
-const val STATE_KEY_QUERY = "recipe.state.query.key"
-const val STATE_KEY_LIST_POSITION = "recipe.state.query.list_position"
-const val STATE_KEY_SELECTED_CATEGORY = "recipe.state.query.selected_category"
-
 @HiltViewModel
 class RecipeListViewModel @Inject constructor(
     private val searchRecipesUsecase: Lazy<SearchRecipesUsecase>,
@@ -229,5 +222,14 @@ class RecipeListViewModel @Inject constructor(
 
     fun toggleDarkTheme() {
         application.changeDarkTheme()
+    }
+
+    companion object {
+        const val PAGE_SIZE = 30
+
+        const val STATE_KEY_PAGE = "recipe.state.page.key"
+        const val STATE_KEY_QUERY = "recipe.state.query.key"
+        const val STATE_KEY_LIST_POSITION = "recipe.state.query.list_position"
+        const val STATE_KEY_SELECTED_CATEGORY = "recipe.state.query.selected_category"
     }
 }
