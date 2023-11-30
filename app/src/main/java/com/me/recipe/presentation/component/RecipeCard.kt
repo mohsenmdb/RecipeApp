d00f5a8c43b54dd2fe50f9980530145f595c781d
Mohse <mohse.mdb@gmail.com>
2023-11-30 18:44:39 +0330

    replace coil instead of glid

--- app/src/main/java/com/me/recipe/presentation/component/RecipeCard.kt
+++ app/src/main/java/com/me/recipe/presentation/component/RecipeCard.kt
@@ -1,8 +1,12 @@
 package com.me.recipe.presentation.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -10,12 +14,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import com.me.recipe.R
 import com.me.recipe.domain.features.recipe.model.Recipe
-import com.me.recipe.util.loadPicture
+import com.me.recipe.presentation.component.image.CoilImage
 
 @Composable
 fun RecipeCard(recipe: Recipe, onClick: () -> Unit) {
@@ -29,21 +31,16 @@ fun RecipeCard(recipe: Recipe, onClick: () -> Unit) {
             .clickable(onClick = onClick),
     ) {
         Column(modifier = Modifier.fillMaxWidth()) {
-            recipe.featuredImage?.let {
-                val image = loadPicture(url = it, defaultImage = R.drawable.empty_plate).value
-                image?.let { img ->
-                    Image(
-                        bitmap = img.asImageBitmap(),
-                        contentDescription = "recipe image",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .requiredHeight(225.dp),
-                        contentScale = ContentScale.Crop
-                    )
-                }
-            }
+            CoilImage(
+                data = recipe.featuredImage,
+                contentDescription = "recipe image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .requiredHeight(225.dp),
+                contentScale = ContentScale.Crop
+            )
 
-            recipe.title?.let { title ->
+            recipe.title.let { title ->
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
--- app/src/main/java/com/me/recipe/presentation/component/RecipeView.kt
+++ app/src/main/java/com/me/recipe/presentation/component/RecipeView.kt
@@ -1,37 +1,35 @@
 package com.me.recipe.presentation.component
 
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import com.me.recipe.R
 import com.me.recipe.domain.features.recipe.model.Recipe
-import com.me.recipe.util.loadPicture
+import com.me.recipe.presentation.component.image.CoilImage
 
 @Composable
 fun RecipeView(recipe: Recipe) {
     LazyColumn(modifier = Modifier.fillMaxWidth()) {
         item {
-            val image =
-                loadPicture(url = recipe.featuredImage, defaultImage = R.drawable.empty_plate).value
-            image?.let { img ->
-                Image(
-                    bitmap = img.asImageBitmap(),
-                    contentDescription = "Recipe Featured Image",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(250.dp),
-                    contentScale = ContentScale.Crop,
-                )
-            }
+            CoilImage(
+                data = recipe.featuredImage,
+                contentDescription = "Recipe Featured Image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp),
+                contentScale = ContentScale.Crop
+            )
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -43,7 +41,7 @@ fun RecipeView(recipe: Recipe) {
                         .padding(bottom = 4.dp)
                 ) {
                     Text(
-                        text = recipe.title!!,
+                        text = recipe.title,
                         modifier = Modifier
                             .fillMaxWidth(0.85f)
                             .wrapContentWidth(Alignment.Start),
